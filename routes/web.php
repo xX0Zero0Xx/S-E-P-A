3c6247d708b1f1f2ffa0a3b2c32774d902718564
xX0Zero0Xx <lozadacesar1218@gmail.com>
2026-08-02 13:19:07 -0600

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckRol;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +14,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', CheckRol::class . ':administrador'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+});
+
+Route::middleware(['auth', CheckRol::class . ':capturista'])->prefix('capturista')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('capturista.dashboard');
+    })->name('capturista.dashboard');
 });
 
 require __DIR__.'/auth.php';
